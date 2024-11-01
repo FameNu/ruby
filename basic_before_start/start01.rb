39229bfa5d000b7703b9c568496ca478e42c1622
FameNu <110712106+FameNu@users.noreply.github.com>
2024-11-01 12:12:56 +0700

    add: variable, function, check class of each variable, and string interpolation

--- basic_before_start/start01.rb
+++ basic_before_start/start01.rb
@@ -1,1 +1,33 @@
 puts "hello, world"
+
+# Variable type
+name = "John" # string
+age = 20 # integer
+height = 1.85 # float
+is_student = false # boolean
+
+# Function to check student
+def check_student(is_student_input)
+  if is_student_input
+    puts "I am a student"
+  else
+    puts "I am not a student"
+  end
+end
+
+# puts with typeof each variable
+puts name.class
+puts age.class
+puts height.class
+puts is_student.class
+check_student(is_student)
+
+is_student = !is_student
+check_student(is_student)
+
+# Functions
+def greeting(name, age)
+  # String interpolation
+  puts "Hello, my name is #{name}, I am #{age} years old."
+end
+greeting(name, age)
